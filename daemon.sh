0cbc4cc8cf0495fd38d21e5beb5b10c7b4c41440
JoseCorCab <josecordoba@uma.es>
2022-09-28 16:33:54 +0200

    Resources corrected and daemon has been modulated

--- daemon.sh
+++ daemon.sh
@@ -2,18 +2,27 @@
 
 source ~soft_bio_267/initializes/init_autoflow
 CODE_PATH=`pwd`
-ADD_OPTIONS=$1
+ADD_OPTIONS=$2
+module=$1
 source $CODE_PATH/config_daemon
 
-AF_VARS=`echo "
-\\$multimir_path=$Multimir_path,
-\\$rnaseq_input_path=$RNAseq_input,
-\\$mirnaseq_input_path=$miRNA_seq_input,
-\\$organism=$Organism,
-\\$ADD_OPT_corr=$Add_opt_corr,
-\\$corr_thrs=$CORR_THR,
-\\$f_organism=$F_organism,
-\\$ADD_OPT_enrichment=$Add_opt_enr
-" | tr -d [:space:]`
 
-AutoFlow -e -w targets_templates.af -V $AF_VARS -o $output $ADD_OPTIONS
+if [ "$module" == "1" ] ; then
+
+
+	AF_VARS=`echo "
+	\\$multimir_path=$Multimir_path,
+	\\$rnaseq_input_path=$RNAseq_input,
+	\\$mirnaseq_input_path=$miRNA_seq_input,
+	\\$organism=$Organism,
+	\\$ADD_OPT_corr=$Add_opt_corr,
+	\\$corr_thrs=$CORR_THR,
+	\\$f_organism=$F_organism,
+	\\$ADD_OPT_enrichment=$Add_opt_enr
+	" | tr -d [:space:]`
+
+	AutoFlow -e -w targets_templates.af -V $AF_VARS -o $output $ADD_OPTIONS
+
+elif [ "$module" == "2" ] ; then
+	$CODE_PATH/launch_functional.sh
+fi
--- launch_functional.sh
+++ launch_functional.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --cpus-per-task=16
+#SBATCH --mem='60gb'
+#SBATCH --constraint=cal
+#SBATCH --time='05:00:00'
+
 source ~soft_bio_267/initializes/init_degenes_hunter
 
 input_path=$f_input_path/results/$strategy
-clusters_to_enrichment.R -i $input_path/miRNA_to_enrich.txt -g $input_path/gene_attributes.txt -o $f_output_path -f $funsys -O $F_organism -k ENSEMBL $Add_opt_enr
-
+clusters_to_enrichment.R -w 16 -i $input_path/miRNA_to_enrich.txt -g $input_path/gene_attributes.txt -o $f_output_path -f $funsys -O $F_organism -k ENSEMBL $Add_opt_enr
