3e665fbbc90cc43344069ce95aeac967c0f34311
JoseCorCab <josecordoba@uma.es>
2022-10-13 21:09:30 +0200

    Functional results corrected

--- launch_functional.sh
+++ launch_functional.sh
@@ -5,7 +5,9 @@
 #SBATCH --time='05:00:00'
 
 source ~soft_bio_267/initializes/init_degenes_hunter
-export PATH=~/software/ExpHunterSuite/inst/scripts/:$PATH
+#export PATH=~/software/ExpHunterSuite/inst/scripts/:$PATH
 
 input_path=$f_input_path/results/$strategy
+mkdir -p $f_output_path
+cp $input_path/all_miRNA_summary.txt $input_path/target_results_table.txt $input_path/../miRNA_target.html $f_output_path/../
 clusters_to_enrichment.R -w 16 -i $input_path/miRNA_to_enrich.txt -g $input_path/gene_attributes.txt -o $f_output_path -f $funsys -O $F_organism -k ENSEMBL $Add_opt_enr
