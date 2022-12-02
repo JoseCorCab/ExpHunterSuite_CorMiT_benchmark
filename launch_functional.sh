0538dadaef3da2710997f7e29259c954f287bbe5
JoseCorCab <josecordoba@uma.es>
2022-12-02 12:11:23 +0100

    Functional enrichment p.adjust threshold has been added

--- launch_functional.sh
+++ launch_functional.sh
@@ -10,4 +10,4 @@ source ~soft_bio_267/initializes/init_degenes_hunter
 input_path=$f_input_path/results/$strategy
 mkdir -p $f_output_path
 cp $input_path/all_miRNA_summary.txt  $input_path/target_results_table.txt $input_path/../hunter_results_table_translated.txt $input_path/../miRNA_target.html $f_output_path/../
-clusters_to_enrichment.R -w 16 -i $input_path/miRNA_to_enrich.txt -g $input_path/gene_attributes.txt -o $f_output_path -f $funsys -O $F_organism -k ENSEMBL $Add_opt_enr
+clusters_to_enrichment.R -p $f_pval -w 16 -i $input_path/miRNA_to_enrich.txt -g $input_path/gene_attributes.txt -o $f_output_path -f $funsys -F -O $F_organism -k ENSEMBL $Add_opt_enr
