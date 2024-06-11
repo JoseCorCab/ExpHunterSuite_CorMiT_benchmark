9a49f394efba1446d0e8372ece1292b36305779f
JoseCorCab <josecordoba@uma.es>
2024-06-11 10:44:38 +0200

    Reporter script modified

--- aux_scripts/create_cormit_report.sh
+++ aux_scripts/create_cormit_report.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -p $CODE_PATH/miRNA_target_results/functional_enrichment
+mkdir $CODE_PATH/miRNA_target_results/functional_enrichment/data_files
+mkdir $CODE_PATH/miRNA_target_results/functional_enrichment/reports
 
-cp $output/clusters_to_enrichment.R_0000/DB_functional/*html $output/clusters_to_enrichment.R_0000/DB_functional/*csv  $CODE_PATH/miRNA_target_results/functional_enrichment/
-cp $output/coRmiT.R_0000/results/target_results_table.txt $CODE_PATH/miRNA_target_results/
+cp $output/clusters_to_enrichment.R_0000/DB_functional/*html $CODE_PATH/miRNA_target_results/functional_enrichment/reports/
+cp $output/clusters_to_enrichment.R_0000/DB_functional/*csv  $CODE_PATH/miRNA_target_results/functional_enrichment/data_files/
+cp $output/coRmiT.R_0000/results/target_results_table.txt $output/coRmiT.R_0000/results/miRNA_target.html $CODE_PATH/miRNA_target_results/
